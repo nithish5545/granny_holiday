1c388ee85e55bd2552027c437d0f62602130832d
nithish5545 <nithimalini10@gmail.com>
2026-03-15 08:56:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "change-me-in-production")
 # --- Database Config ---
 DB_CONFIG = {
     "host":     os.environ.get("DB_HOST",     "3rgacs.h.filess.io"),
-    "port":     int(os.environ.get("DB_PORT", "61001")),
+    "port":     int(os.environ.get("DB_PORT", " 3306")),
     "user":     os.environ.get("DB_USER",     "tour_planner_structure"),
     "password": os.environ.get("DB_PASSWORD", "311dfe5048531cdd1260b94b80c3a2781de2bb62"),
     "database": os.environ.get("DB_NAME",     "tour_planner_structure"),
